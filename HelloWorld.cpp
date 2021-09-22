887edd3b1f8f1085843a3433c22b74d5477a08bb
Ian Wixson <fy3838@wayne.edu>
2021-09-22 13:05:03 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,6 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+//Author: Ian Wixson <fy3838@wayne.edu>
+
 // Header file for input output functions 
 #include<iostream>  
   
